34f3b3282a00a9eb3ad2026c063a25858c39820b
chiggy69 <chiragaggarwal69@gmail.com>
2023-02-02 16:45:49 +0530

    ✨ 3D Text

--- drei/src/Experience.jsx
+++ drei/src/Experience.jsx
@@ -5,10 +5,12 @@ import {
 	TransformControls,
 	PivotControls,
 	Html,
+	Text,
 } from "@react-three/drei";
 
 const Experience = () => {
 	const cubeRef = useRef();
+	const sphereRef = useRef();
 
 	useFrame((state, delta) => {
 		cubeRef.current.rotation.y += delta;
@@ -33,10 +35,10 @@ const Experience = () => {
 				depthTest={false} // to show the axis even if the object is behind another object
 				lineWidth={4}
 				axisColors={["#9381ff", "#ff4d6d", "#7ae582"]} // color for axis
-				scale={100} // initially respective to object's scale but after fixed=true scale is in pixels
+				scale={40} // initially respective to object's scale but after fixed=true scale is in pixels
 				fixed={true} //to remove the default perspective behaviour
 			>
-				<mesh scale={0.7} position={[-3, 0, 0]}>
+				<mesh scale={0.7} position={[-3, 0, 0]} ref={sphereRef}>
 					<sphereGeometry />
 					<meshStandardMaterial color="orange" wireframe={false} />
 					<Html
@@ -44,6 +46,7 @@ const Experience = () => {
 						position={[1, 1, 0]} // position relative to the object
 						center // pivot point is the center of the Html element
 						distanceFactor={8} //to simulate perspective behaviour
+						// occlude={[cubeRef, sphereRef]} // to hide the Html element if the object is behind another object
 					>
 						Sphere
 					</Html>
@@ -54,6 +57,11 @@ const Experience = () => {
 				<planeGeometry args={[15, 15]} />
 				<meshStandardMaterial color="greenyellow" wireframe={false} />
 			</mesh>
+
+			<Text maxWidth={3} textAlign="center">
+				I Love R3F
+				<meshNormalMaterial />
+			</Text>
 		</>
 	);
 };
