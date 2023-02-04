b2796a6b88627b297da6ea618ee90602a3a151f1
chiggy69 <chiragaggarwal69@gmail.com>
2023-02-04 15:59:10 +0530

    🧑‍💻 useStrict mode usage

--- debug/src/main.jsx
+++ debug/src/main.jsx
@@ -3,17 +3,20 @@ import ReactDOM from "react-dom/client";
 import { Canvas } from "@react-three/fiber";
 import "./index.css";
 import Experience from "./Experience";
+import { StrictMode } from "react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-	<Canvas
-		alpha="true"
-		camera={{
-			near: 0.1,
-			far: 1000,
-			fov: 75,
-			position: [3, 2, 9],
-		}}
-	>
-		<Experience />
-	</Canvas>
+	<StrictMode>
+		<Canvas
+			alpha="true"
+			camera={{
+				near: 0.1,
+				far: 1000,
+				fov: 75,
+				position: [3, 2, 9],
+			}}
+		>
+			<Experience />
+		</Canvas>
+	</StrictMode>
 );
