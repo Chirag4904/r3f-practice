529073996712e8d09ccafe18c6a6015824ea6bee
chiggy69 <chiragaggarwal69@gmail.com>
2023-02-05 17:54:55 +0530

    🎉 Environment and Staging

---
+++ Environment And Staging/src/main.jsx
@@ -0,0 +1,23 @@
+import ReactDOM from "react-dom/client";
+import { Canvas } from "@react-three/fiber";
+import "./index.css";
+import Experience from "./Experience";
+import { StrictMode } from "react";
+import { Leva } from "leva";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<StrictMode>
+		<Leva collapsed={true} />
+		<Canvas
+			alpha="true"
+			camera={{
+				near: 0.1,
+				far: 1000,
+				fov: 75,
+				position: [3, 2, 9],
+			}}
+		>
+			<Experience />
+		</Canvas>
+	</StrictMode>
+);
