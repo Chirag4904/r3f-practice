4f3d4829d8d8e21f9c11adbb7d64acb7884b4998
chiggy69 <chiragaggarwal69@gmail.com>
2023-02-05 18:33:06 +0530

    useHelper hook and adding light helper

--- Environment And Staging/src/Experience.jsx
+++ Environment And Staging/src/Experience.jsx
@@ -1,11 +1,15 @@
 import { useFrame } from "@react-three/fiber";
 import { useRef } from "react";
-import { OrbitControls } from "@react-three/drei";
+import { OrbitControls, useHelper } from "@react-three/drei";
 import { Perf } from "r3f-perf";
+import * as THREE from "three";
 
 const Experience = () => {
 	const cubeRef = useRef();
 	const sphereRef = useRef();
+	const directionalLight = useRef();
+
+	useHelper(directionalLight, THREE.DirectionalLightHelper, 0.5, "hotpink"); //useHelper is a hook that allows you to add helpers to objects where first argument is the object you want to add the helper to, second argument is the helper class from threejs you want to add, third argument is the size of the helper, and fourth argument is the color of the helper
 
 	useFrame((state, delta) => {
 		cubeRef.current.rotation.y += delta;
@@ -18,7 +22,7 @@ const Experience = () => {
 			<OrbitControls
 				makeDefault // when using multiple controls, only one should be default so that the other controls can work
 			/>
-			<directionalLight position={[1, 2, 3]} />
+			<directionalLight ref={directionalLight} position={[1, 2, 3]} />
 			<ambientLight intensity={0.3} />
 
 			<mesh position={[2, 0, 0]} ref={cubeRef}>
