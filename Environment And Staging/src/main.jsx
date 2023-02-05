2913a77eb5a35706c616e66083040bc68a0a3502
chiggy69 <chiragaggarwal69@gmail.com>
2023-02-05 18:26:45 +0530

    ✨ 4 ways to add background color to the scene

--- Environment And Staging/src/main.jsx
+++ Environment And Staging/src/main.jsx
@@ -2,13 +2,23 @@ import ReactDOM from "react-dom/client";
 import { Canvas } from "@react-three/fiber";
 import "./index.css";
 import Experience from "./Experience";
-import { StrictMode } from "react";
+import * as THREE from "three";
+
 import { Leva } from "leva";
+const created = (state) => {
+	//Option 2 to set background color is to use webgl renderer and setClearColor
+	// const { gl } = state;
+	// gl.setClearColor("#ff0000", 0.5);
+	//OPTION 3 to set background color is to use scene and set background
+	// const { scene } = state;
+	// scene.background = new THREE.Color("#ff0000");
+};
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-	<StrictMode>
+	<>
 		<Leva collapsed={true} />
 		<Canvas
+			onCreated={created}
 			alpha="true"
 			camera={{
 				near: 0.1,
@@ -17,7 +27,9 @@ ReactDOM.createRoot(document.getElementById("root")).render(
 				position: [-7, 3, 5],
 			}}
 		>
+			{/* Option4 to set background color is to create a color using R3F and attach it to the scene's background
+			<color args={["#ff0000"]} attach="background" /> */}
 			<Experience />
 		</Canvas>
-	</StrictMode>
+	</>
 );
